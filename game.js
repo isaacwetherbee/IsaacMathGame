cad4889ccdaa611d66feaed3d14d4bd41f046cb7
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-09 13:25:40 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let x = 375;
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(x, 560, 50, 20);
+
+  x += 2;
+  if (x > canvas.width) x = -50;
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
