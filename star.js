f0fb1807304d8fc05f0869be4652da914123bd14
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-10 13:30:10 -0500

    Update star.js

--- star.js
+++ star.js
@@ -1,1 +1,24 @@
-HelloWorld
+class Star {
+    constructor(panelWidth, panelHeight) {
+        this.x = Math.floor(Math.random() * panelWidth);
+        this.y = Math.floor(Math.random() * panelHeight);
+        this.size = Math.floor(Math.random() * 3) + 1;   // 1–3 pixels
+        this.speed = Math.floor(Math.random() * 2) + 1;  // 1–2 pixels per frame
+    }
+
+    update(panelWidth, panelHeight) {
+        this.y += this.speed;
+
+        if (this.y > panelHeight) {
+            this.y = 0;
+            this.x = Math.floor(Math.random() * panelWidth);
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "yellow";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
