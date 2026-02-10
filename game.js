82103c418120c00b1f9652a127f7aa78e2eadf11
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-10 13:38:01 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+/*
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -248,3 +249,23 @@ function spawnProblem() {
 }
 
 window.onload = init;
+*/
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+console.log("Game loaded");
+
+function loop() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(100, 100, 50, 50);
+
+    requestAnimationFrame(loop);
+}
+
+loop();
