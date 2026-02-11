f6f6bcf5cabd90400bb967352b4a172389af8b04
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-11 14:47:11 -0500

    Fix comment formatting in game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ let gameOver = false;
 canvas.width = 800;   // desired width
 canvas.height = 600;  // desired height
 
-//projectile variables
+//projectile variables  
 let projectiles = []; // array to hold Projectile objects
 let lastShotTime = 0;
 const SHOOT_COOLDOWN_MS = 100; // 100ms cooldown between shots
