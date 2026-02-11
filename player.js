67e7b88ef23c1ba7bb2564192b4753ec92b7aa67
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-11 13:40:16 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,5 +1,4 @@
 class player {
-
   constructor(x = 0, y = 0) {
         this.position = {
             x: x,
@@ -26,69 +25,46 @@ class player {
         return this.size;
     }
 
+    get position() {
+        return this._position;
+    }
+    set position(value) {
+        this._position = value;
+    }
+
+    setX(x) {
+        this.position.x = x;
+    }
+
+    setY(y) {
+        this.position.y = y;
+    }
+
+    draw(ctx) {
+        const x = this.position.x;
+        const y = this.position.y;
+        const half = this.size / 2;
+
+        // Draw triangle pointing up
+        ctx.beginPath();
+        ctx.moveTo(x, y - half);        // top point
+        ctx.lineTo(x - half, y + half); // bottom left
+        ctx.lineTo(x + half, y + half); // bottom right
+        ctx.closePath();
+
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+    }
 
   
-  /*
-  constructor(x = 0, y = 0) {
-    this.position = { x, y };
-    this.size = 20; // triangle size
-  } 
-
-  // ======================
-  // Movement
-  // ======================
-  move(dx, dy) {
-    this.position.x += dx;
-    this.position.y += dy;
-  }
-
-  setPosition(x, y) {
-    this.position.x = x;
-    this.position.y = y;
-  }
-
-  // ======================
-  // Getters
-  // ======================
-  getX() {
-    return this.position.x;
-  }
-
-  getY() {
-    return this.position.y;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  getBounds() {
-    return {
-      x: this.position.x - 10,
-      y: this.position.y - 10,
-      w: 20,
-      h: 20
-    };
-  }
-
-  // ======================
-  // Draw
-  // ======================
-  draw(ctx) {
-    const x = this.position.x;
-    const y = this.position.y;
-    const half = this.size / 2;
-
-    ctx.fillStyle = "blue";
-    ctx.beginPath();
-    ctx.moveTo(x, y - half);
-    ctx.lineTo(x - half, y + half);
-    ctx.lineTo(x + half, y + half);
-    ctx.closePath();
-    ctx.fill();
-  }
-  */
-
+    getBounds() {
+        return {
+            x: this.position.x - 10,
+            y: this.position.y - 10,
+            width: 20,
+            height: 20
+        };
+    }
 
     
 }
