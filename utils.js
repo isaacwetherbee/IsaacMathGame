181d7e84fdb957a766a99be7c55e2b55491e0a01
isaacwetherbee <isaac.wetherbee@gmail.com>
2026-02-11 09:57:45 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,12 +1,14 @@
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+class utils{
+  function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-function rectsIntersect(a, b) {
-  return (
-    a.x < b.x + b.w &&
-    a.x + a.w > b.x &&
-    a.y < b.y + b.h &&
-    a.y + a.h > b.y
-  );
+  function rectsIntersect(a, b) {
+    return (
+      a.x < b.x + b.w &&
+      a.x + a.w > b.x &&
+      a.y < b.y + b.h &&
+      a.y + a.h > b.y
+    );
+  }
 }
